f081362370e1cb6fb88a4d7363ec111cc8d78fc0
KacperJarocki <dev.kacperjarocki@proton.me>
2024-07-28 16:12:14 +0200

    accept incoming connections

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,13 @@ impl HttpServer {
         Self {
             socket: match TcpListener::bind(address) {
                 Ok(listener) => listener,
-                Err(e) => panic!("Failed to bind to port: {}", e),
+                Err(e) => {
+                    println!("Failed to bind to port: {}", e);
+                    let s = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0))
+                        .unwrap();
+                    println!("bind to port: {}", s.local_addr().unwrap().port());
+                    s
+                }
             },
         }
     }
@@ -21,13 +27,7 @@ impl HttpServer {
     }
 }
 #[test]
-fn when_port_is_free_should_create_proper_listener() {
-    let server: HttpServer = HttpServer::run(8080);
-    assert_eq!(server.socket.local_addr().unwrap().port(), 8080);
-}
-#[test]
-#[should_panic]
-fn when_port_is_taken_creating_should_panic() {
+fn when_called_will_create_listener_even_if_port_taken() {
     let _server1: HttpServer = HttpServer::run(8080);
     let _server2: HttpServer = HttpServer::run(8080);
 }
@@ -36,17 +36,13 @@ fn when_proper_listener_is_created_should_accept_connection() {
     use std::net::TcpStream;
     use std::thread;
     use std::time::Duration;
-
-    // Załóżmy, że HttpServer implementuje metodę `run` uruchamiającą serwer.
     let server: HttpServer = HttpServer::run(8000);
-
-    // Uruchomienie wątku, który będzie próbował nawiązać połączenie.
+    let port = server.socket.local_addr().unwrap().port();
+    let address = format!("127.0.0.1:{}", port);
     thread::spawn(move || {
-        // Czekamy krótko, aby dać czas serwerowi na uruchomienie.
         thread::sleep(Duration::from_millis(100));
-        let _connection = TcpStream::connect("127.0.0.1:8000").expect("Failed to connect");
+        let _connection = TcpStream::connect(address).expect("Failed to connect");
     });
 
-    // Serwer powinien zaakceptować połączenie.
     let _accept = server.accept().expect("Failed to accept connection");
 }
