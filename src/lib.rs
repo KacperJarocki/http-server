e80dcd74936a9db02b9e2696fb3a6f5b4de138e8
KacperJarocki <dev.kacperjarocki@proton.me>
2024-07-30 14:56:21 +0200

    new struct http_request

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,93 @@
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream};
-
-pub struct HttpServer {
-    socket: TcpListener,
-}
-impl HttpServer {
-    pub fn run(port: u16) -> Self {
-        let address = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port);
-        Self {
-            socket: match TcpListener::bind(address) {
-                Ok(listener) => listener,
-                Err(e) => {
-                    println!("Failed to bind to port: {}", e);
-                    let s = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0))
-                        .unwrap();
-                    println!("bind to port: {}", s.local_addr().unwrap().port());
-                    s
-                }
-            },
+pub mod http_request {
+    pub enum HttpMethod {
+        Get,
+        Post,
+        Put,
+        Delete,
+        Patch,
+        Trace,
+        Options,
+        Connect,
+    }
+    pub struct HttpRequest {
+        method: HttpMethod,
+        route: String,
+    }
+    impl HttpRequest {
+        pub fn create(method: HttpMethod, route: String) -> Self {
+            Self { method, route }
+        }
+        pub fn get_method(&self) -> &HttpMethod {
+            &self.method
+        }
+        pub fn get_route(&self) -> &String {
+            &self.route
         }
     }
-    pub fn accept(&self) -> Option<(TcpStream, SocketAddr)> {
-        match self.socket.accept() {
-            Ok((socket, accept)) => Some((socket, accept)),
-            Err(_) => None,
+}
+pub mod server {
+    use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream};
+    pub struct HttpServer {
+        socket: TcpListener,
+    }
+    impl HttpServer {
+        pub fn run(port: u16) -> Self {
+            let address = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port);
+            Self {
+                socket: match TcpListener::bind(address) {
+                    Ok(listener) => listener,
+                    Err(e) => {
+                        println!("Failed to bind to port: {}", e);
+                        let s =
+                            TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0))
+                                .unwrap();
+                        println!("bind to port: {}", s.local_addr().unwrap().port());
+                        s
+                    }
+                },
+            }
+        }
+        pub fn accept(&self) -> Option<(TcpStream, SocketAddr)> {
+            match self.socket.accept() {
+                Ok((stream, accept)) => Some((stream, accept)),
+                Err(_) => None,
+            }
+        }
+        pub fn local_addr(&self) -> SocketAddr {
+            self.socket.local_addr().unwrap()
         }
     }
-    pub fn local_addr(&self) -> SocketAddr {
-        self.socket.local_addr().unwrap()
+    #[test]
+    fn when_called_will_create_listener_even_if_port_taken() {
+        let _server1: HttpServer = HttpServer::run(8080);
+        let _server2: HttpServer = HttpServer::run(8080);
     }
-}
-#[test]
-fn when_called_will_create_listener_even_if_port_taken() {
-    let _server1: HttpServer = HttpServer::run(8080);
-    let _server2: HttpServer = HttpServer::run(8080);
-}
-#[test]
-fn when_proper_listener_is_created_should_accept_connection() {
-    use std::net::TcpStream;
-    use std::thread;
-    use std::time::Duration;
-    let server: HttpServer = HttpServer::run(23000);
-    let port = server.socket.local_addr().unwrap().port();
-    let address = format!("127.0.0.1:{}", port);
-    thread::spawn(move || {
-        thread::sleep(Duration::from_millis(100));
-        let connection = TcpStream::connect(address).expect("Failed to connect");
-        assert_eq!(
-            connection.peer_addr().unwrap(),
-            SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080))
-        );
-    });
-    let address = format!("127.0.0.1:{}", port);
-    thread::spawn(move || {
-        thread::sleep(Duration::from_millis(100));
-        let connection = TcpStream::connect(address).expect("Failed to connect");
-        assert_eq!(
-            connection.peer_addr().unwrap(),
-            SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080))
-        );
-    });
+    #[test]
+    fn when_proper_listener_is_created_should_accept_connection() {
+        use std::net::TcpStream;
+        use std::thread;
+        use std::time::Duration;
+        let server: HttpServer = HttpServer::run(23000);
+        let port = server.socket.local_addr().unwrap().port();
+        let address = format!("127.0.0.1:{}", port);
+        thread::spawn(move || {
+            thread::sleep(Duration::from_millis(100));
+            let connection = TcpStream::connect(address).expect("Failed to connect");
+            assert_eq!(
+                connection.peer_addr().unwrap(),
+                SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port))
+            );
+        });
+        let address = format!("127.0.0.1:{}", port);
+        thread::spawn(move || {
+            thread::sleep(Duration::from_millis(100));
+            let connection = TcpStream::connect(address).expect("Failed to connect");
+            assert_eq!(
+                connection.peer_addr().unwrap(),
+                SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port))
+            );
+        });
 
-    let _accept = server.accept().expect("Failed to accept connection");
+        let _accept = server.accept().expect("Failed to accept connection");
+    }
 }
